d4f59d60235a395cf994b0eb14925ef028b12d0a
Sohan Reddy <gantalasohanreddy@gmail.com>
2024-09-17 14:27:14 -0500

    local change

--- api/asgi.py
+++ api/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+#test comming
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
